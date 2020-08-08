d6edda1695eebf7146db2b10bd3c85d703bff3b0
Pattapol154 <sunfly-210@hotmail.com>
2020-08-09 05:16:50 +0700

    add list code

--- data/lists.js
+++ data/lists.js
@@ -1,74 +1,40 @@
 export const projects = [
   {
-    icon: "🗺️",
-    name: "Blocks",
-    description: "Location based capture the flag, multiplayer PWA.",
-    source: "https://github.com/Jonathannsegal/blocks",
-    live: "https://blocks.jonathansegal.now.sh/",
+    icon: "🧑🏻‍💻 👨🏻‍💻",
+    name: "landing-page",
+    description: "boostrap5-landing-page",
+    source: "https://github.com/Pattapol154/boostrap5-LandingPage.git",
+    live: "thirsty-engelbart-a284d2.netlify.app",
   },
   {
-    icon: "🏦",
-    name: "Crypto Bonds",
+    icon: "👨🏼‍🔬 👩🏼‍🔬",
+    name: "react-breaking bad",
     description:
-      "A platform to transfer crypto currency backed by bonds on the blockchain.",
-    source: "https://github.com/crypto-bonds",
-    live: "http://cryptobonds.tech/",
+      "my-api-react",
+    source: "https://github.com/Pattapol154/react-api-breakingbad.git",
+    live: "https://pattapol154.github.io/react-api-breakingbad/",
   },
   {
-    icon: "🏒",
-    name: "Cloud Hockey",
-    description: "I taped a phone to a hocket stick...",
-    source: "https://github.com/cloud-hockey",
+    icon: "🕵🏼‍♂️ 🕵🏼‍♀️",
+    name: "material-ui-blog-react",
+    description: "blog react",
+    source: "https://github.com/Pattapol154/react-blog-MaterialUI.git",
+    live: "https://pattapol154.github.io/react-blog/",
   },
   {
-    icon: "👨‍💻",
-    name: "Git a Dev",
-    description: "Searching for developers near you?",
-    source: "https://github.com/Jonathannsegal/git_a_dev",
+    icon: " 🦸🏼‍♂️",
+    name: "vueJS",
+    description: "weather-vue",
+    source: "https://github.com/Pattapol154/vue-weather-app.git",
+    live: "https://keen-jones-aeebcc.netlify.app/",
   },
   {
-    icon: "🏭",
-    name: "Carbon Adventure",
+    icon: "🧑🏻‍🎓 ",
+    name: "chakra-ui-app",
     description:
-      "A simulation of how decisions impact our global carbon footprint.",
-    source: "https://github.com/Jonathannsegal/CarbonAdventure",
-  },
-  {
-    icon: "🐠",
-    name: "Splooosh",
-    description:
-      "A game where you are a giant floating fish. Need I say more..",
-    source: "https://github.com/Jonathannsegal/sploosh",
-  },
-  {
-    icon: "🏠",
-    name: "Spring Home",
-    description: "Just a slightly below average HTML5 platformer.",
-    source: "https://github.com/Jonathannsegal/spring_home",
-    live: "https://springhome.herokuapp.com/menu/",
-  },
-  {
-    icon: "🏓",
-    name: "Face Pong",
-    description:
-      "Real time face tracking where your face is used as a pong paddle.",
-    source: "https://github.com/Jonathannsegal/face_pong",
-    live: "https://jonathannsegal.github.io/face_pong/",
-  },
-  {
-    icon: "🚴",
-    name: "Raspberry Pike",
-    description:
-      "Internet connected workouts, earn time to spend on the internet based on your workout.",
-    source: "https://github.com/Jonathannsegal/raspberry_pike",
-  },
-  {
-    icon: "🕹️",
-    name: "Power Up",
-    description:
-      "Remember the Ouya? Well I made this game for the Ouya. It's a dual stick shooter.",
-    source: "https://github.com/Jonathannsegal/powerup",
-    download: "../data/powerupwin.zip",
+      "Portfolio-app",
+    source: "https://github.com/Pattapol154/pattapol.io.git",
+    live: "pattapol-io.vercel.app"
   },
 ];
 
